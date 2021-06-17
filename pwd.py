9553b545b72d522c6071bc82c5a65725f28ab7f7
Ann Chen <ann9975656@gmail.com>
2021-06-18 02:38:14 +0800

    老師版

--- pwd.py
+++ pwd.py
@@ -1,17 +1,11 @@
-x = 0
+password = 'a12345'
+x = 3
 while True:
-	password = input('請輸入密碼:')
-	if password == 'a123456':
+	pwd = input('請輸入密碼：')
+	if pwd == password:
 		print('登入成功')
-		break
-	else: 
+	else:
+		x = x - 1
+		print('登入錯誤！ 還有', x, '次機會')
 		if x == 0:
-			print('密碼錯誤 還有兩次機會')
-		elif x == 1:
-			print('密碼錯誤 還有一次機會')
-		else:
-			print('帳號鎖住')
-			break
-		x = x + 1
-
-	
+			break 
